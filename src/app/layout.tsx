ed47fe07afa068d92bb5b2fc36f527482b579ff4
Andres Rojas <arojasc30.dev@gmail.com>
2025-08-24 21:34:59 -0600

    Add cache provider and clean code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import ThemeRegistry from "./theme-registry";
-import Box from "@mui/material/Box";
-import Link from "next/link";
+import NavBar from "@/components/NavBar";
 
 export const metadata: Metadata = {
   title: "Enescena",
@@ -12,14 +11,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <ThemeRegistry>
-          <Box sx={{ p: 2, display: "flex", gap: 2 }}>
-            <Link href="/api/auth/login">Login</Link>
-            <Link href="/api/auth/logout">Logout</Link>
-            <Link href="/dashboard">Dashboard</Link>
-          </Box>
-          {children}
-        </ThemeRegistry>
+        <NavBar />
+        <ThemeRegistry>{children}</ThemeRegistry>
       </body>
     </html>
   );
